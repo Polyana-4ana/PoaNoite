418ef9553feed4c718762f04cd3f5d40854573d8
lacerda <gabriellacerda.gl2006@gmail.com>
2026-05-18 10:36:45 -0300

    feat: reorganizando camadas

--- poanoite/src/main/java/polyana/example/poanoite/repository/CasaDeFestaRepository.java
+++
@@ -1,9 +0,0 @@
-package polyana.example.poanoite.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import polyana.example.poanoite.domain.LocalEvento;
-
-public interface CasaDeFestaRepository extends JpaRepository<LocalEvento, Long>,
-        JpaSpecificationExecutor<LocalEvento> {
-}
